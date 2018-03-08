dee28123e74bb43c5cffd44cf1467d7bff0a82de
Alejok28 <alejo_k28@hotmail.com>
2018-03-08 18:17:39 -0500

    Filters a list of posts

--- src/App.js
+++ src/App.js
@@ -5,17 +5,48 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts,
+      keyword: ''
+    }
+  }
+
+  filteredPosts(word) {
+    return this.state.posts.filter( post => {
+      return post.title.toLowerCase().indexOf(word.toLowerCase()) !== -1
+    })
+  }
+
+  handleChange(e) {
+    const word = e.target.value
+    this.setState({
+      keyword: word,
+      posts: word ? this.filteredPosts(word) :posts
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input
+            type="text"
+            placeholder="Ingresa el término de búsqueda"
+            value={this.state.keyword}
+            onChange={this.handleChange.bind(this)}
+          />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {this.state.posts.map( post => {
+            return (
+              <li key={post.title}>
+                <a href={post.url}><img alt={post.title} src={post.image } /></a>
+                <p>{ post.title }</p>
+              </li>
+            )
+          })}
         </ul>
       </div>
     )
@@ -24,5 +55,3 @@ class App extends Component {
 
 
 export default App
-
-
